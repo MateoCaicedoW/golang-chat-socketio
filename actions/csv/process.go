a09e2f30e51b481d67408a1410696d018332966e
MateoCaicedoW <mcaicedo@wawand.co>
2023-09-18 14:32:10 -0500

    tasK: reding from http.Request

--- actions/csv/process.go
+++ actions/csv/process.go
@@ -2,12 +2,10 @@ package csv
 
 import (
 	"encoding/csv"
-	"io"
+	"fmt"
 	"main/render"
 	"net/http"
-	"os"
 	"path"
-	"sync"
 
 	"github.com/tealeg/xlsx"
 )
@@ -29,53 +27,40 @@ func Process(w http.ResponseWriter, r *http.Request) {
 
 	defer file.Close()
 
-	excelFile, err := os.Create(handler.Filename)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//copy the uploaded file to the destination file
-	if _, err := io.Copy(excelFile, file); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	//this is to save the error and the position where it happened
+	errorss := []string{}
 
 	if path.Ext(handler.Filename) == ".csv" {
-		// read the csv file
-		a, err := os.Open(handler.Filename)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
 
-		defer a.Close()
-		csvFile := csv.NewReader(a)
+		// add this csv data to a matrix
+		// and pass it to the template
+		// to be rendered in a table
+		csvFile := csv.NewReader(file)
 		records, err := csvFile.ReadAll()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		for _, v := range records {
-			for _, v2 := range v {
-				if v2 == "" {
-					v2 = "empty"
+		for i, v := range records {
+			for j, v := range v {
+				if v == "" {
+					records[i][j] = "empty"
+					errorss = append(errorss, fmt.Sprintf("row %d, column %d is empty", i+1, j+1))
 				}
 			}
 		}
 
-		_ = os.Remove(handler.Filename)
+		for _, v := range errorss {
+			fmt.Println(v)
+		}
+
 		render.SetData("matrix", records)
 		render.RenderWithLayout(w, "/csv/process.html", "auth.html")
 		return
 	}
 
-	// add this csv data to a matrix
-	// and pass it to the template
-	// to be rendered in a table
-
-	xlsx, err := xlsx.OpenFile(handler.Filename)
+	xlsx, err := xlsx.OpenReaderAt(file, handler.Size)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -84,47 +69,62 @@ func Process(w http.ResponseWriter, r *http.Request) {
 	// add this xlsx data to a matrix
 	// and pass it to the template
 	// to be rendered in a table
-	var wg sync.WaitGroup
-	matrixChannel := make(chan [][]string, len(xlsx.Sheets))
-
+	var matrix [][]string
 	for _, sheet := range xlsx.Sheets {
-		wg.Add(1)
-		go processSheet(sheet, matrixChannel, &wg)
+		for rowIndex, row := range sheet.Rows {
+			var rowSlice []string
+			for cellIndex, cell := range row.Cells {
+				text := cell.String()
+				if text == "" {
+					text = "empty"
+					errorss = append(errorss, fmt.Sprintf("row %d, column %d is empty", rowIndex+1, cellIndex+1))
+				}
+				rowSlice = append(rowSlice, text)
+			}
+			matrix = append(matrix, rowSlice)
+		}
 	}
 
-	go func() {
-		wg.Wait()
-		close(matrixChannel)
-	}()
-
-	var finalMatrix [][]string
-	for matrix := range matrixChannel {
-		finalMatrix = append(finalMatrix, matrix...)
+	for _, v := range errorss {
+		fmt.Println(v)
 	}
 
-	err = os.Remove(handler.Filename)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	render.SetData("matrix", finalMatrix)
+	// var wg sync.WaitGroup
+	// matrixChannel := make(chan [][]string, len(xlsx.Sheets))
+
+	// for _, sheet := range xlsx.Sheets {
+	// 	wg.Add(1)
+	// 	go processSheet(sheet, matrixChannel, &wg)
+	// }
+
+	// go func() {
+	// 	wg.Wait()
+	// 	close(matrixChannel)
+	// }()
+
+	// var finalMatrix [][]string
+	// for matrix := range matrixChannel {
+	// 	finalMatrix = append(finalMatrix, matrix...)
+	// }
+
+	render.SetData("matrix", matrix)
 	render.RenderWithLayout(w, "/csv/process.html", "auth.html")
 }
 
-func processSheet(sheet *xlsx.Sheet, matrixChannel chan [][]string, wg *sync.WaitGroup) {
-	defer wg.Done()
+// func processSheet(sheet *xlsx.Sheet, matrixChannel chan [][]string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
 
-	var matrix [][]string
-	for _, row := range sheet.Rows {
-		var rowSlice []string
-		for _, cell := range row.Cells {
-			text := cell.String()
-			if text == "" {
-				text = "empty"
-			}
-			rowSlice = append(rowSlice, text)
-		}
-		matrix = append(matrix, rowSlice)
-	}
-	matrixChannel <- matrix
-}
+// 	var matrix [][]string
+// 	for _, row := range sheet.Rows {
+// 		var rowSlice []string
+// 		for _, cell := range row.Cells {
+// 			text := cell.String()
+// 			if text == "" {
+// 				text = "empty"
+// 			}
+// 			rowSlice = append(rowSlice, text)
+// 		}
+// 		matrix = append(matrix, rowSlice)
+// 	}
+// 	matrixChannel <- matrix
+// }
